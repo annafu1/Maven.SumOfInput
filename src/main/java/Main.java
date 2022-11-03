d5b1e5b100883569d4315aa9cfda5efdc076655f
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-11-03 15:05:49 -0400

    program prints the sum of the number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner number = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        Integer n = Integer.parseInt(number.nextLine());
+        System.out.println( 1 + n);
 
     }
     
     int sumOfNumbers(int n) {
-        return 0;
+        return n;
     }
-    
     // for Extra Credit
     int gaussianSumOfNumbers(int n) {
         return 0;
